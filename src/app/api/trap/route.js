ee65f61421d84207a90bd1879c1b0b9a94ffc64a
jyang <jerry.yang@zoom.us>
2024-09-27 04:42:45 -0700

    7

--- src/app/api/trap/route.js
+++ src/app/api/trap/route.js
@@ -1,12 +1,5 @@
-import db from '@/lib/db'
-
 export async function GET(req, res) {
-    // preform a db query to fetch all traps
-    const items = await db.all("SELECT * FROM traps");
-
-    // Return the items as a JSON response with status 200
-    return new Response(JSON.stringify(items), {
-        headers: { "Content-Type": "application/json" },
+    return new Response(JSON.stringify("API success!"), {
         status: 200,
     });
 }
